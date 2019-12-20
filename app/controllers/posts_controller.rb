b538022442a87902d19ca3e834fcefe5d7862014
risa <az.5s.fmt@gmail.com>
2019-12-20 23:36:34 +0900

    Strong Parametersの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,13 @@ class PostsController < ApplicationController
     end
 
     def create
-        
+        @post = Post.new(post_params)
+        @post.save
+        redirect_to root_path
     end
+
+    private
+        def post_params
+            params.require(:post).permit(:title, :body)
+        end
 end
