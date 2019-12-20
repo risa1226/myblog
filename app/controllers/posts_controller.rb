2652e07b0abcee0b39b16bd6d9259f93fc642201
risa <az.5s.fmt@gmail.com>
2019-12-21 00:00:20 +0900

    エラーメッセージの表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,13 +8,16 @@ class PostsController < ApplicationController
     end
 
     def new
-        
+        @post = Post.new
     end
 
     def create
         @post = Post.new(post_params)
-        @post.save
-        redirect_to root_path
+        if @post.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
     end
 
     private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 3, messages: 'Too short to post!'}
+    validates :body, presence: true
 end
